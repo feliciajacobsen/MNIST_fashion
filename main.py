a308c310f62b39bf2d739e566f8cf88d9fcff725
feliciajacobsen <felicia_1997@hotmail.com>
2021-04-24 14:38:56 +0200

    adding train method to cnn

--- main.py
+++ main.py
@@ -57,17 +57,74 @@ class Network(nn.Module):
         # dont add softmax of output since we use CrossEntropy later
         return self.output(x)
 
-    def train(model, dataloader, num_epochs, lr, device):
+    def train(model, dataloader, criterion, optimizer, epochs, device):
         model.to(device)
-        
+
         dataiter = iter(dataloader)
         images, labels = dataiter.next()
 
-        optimizer = optim.SGD(model.parameters(), lr=lr)
-        loss = nn.CrossEntropyLoss()
-        scheduler = optim.lr_sceduler.StepLR(optimizer, steo_size=10, gamma=0.1)
+        losses = []
+        for epoch in range(epochs):
+            for images, labels in dataloader:
+                # Flatten image
+                images = images.view(images.shape[0],-1).to(device)
+                labels = labels.to(device)
+                optimizer.zero_grad()
 
-        for epoch in range(num_epochs):
+                output = model.forward(images)
+                loss = criterion(output, labels)
+                loss.backward()
+                optimizer.step()
+                losses.append(loss.item())
+        return np.mean(losses)
+
+    def train_eval(model, dataloader_train, dataloader_test, criterion, optimizer, epochs, device):
+        dataiter = iter(dataloader)
+        images, labels = dataiter.next()
+
+        for epoch in range(epochs):
+            losses = []
+             for images, labels in trainloader:
+                 # Flatten Fashion-MNIST images into a 784 long vector
+                 images = images.view(images.shape[0], -1)
+
+                 # Training pass
+                 optimizer.zero_grad()
+
+                 output = model.forward(images)
+                 loss = criterion(output, labels)
+                 loss.backward()
+                 optimizer.step()
+
+                 running_losses.append(loss.item())
+            else:
+                test_losses = []
+                accuracy = []
+
+                # Turn off gradients for validation, saves memory and computation
+                with torch.no_grad():
+                  # Set the model to evaluation mode
+                  model.eval()
+
+                  # Validation pass
+                  for images, labels in dataloader_test:
+                    images = images.view(images.shape[0], -1)
+                    log_ps = model(images)
+                    test_losses.append(criterion(log_ps, labels))
+
+                    ps = torch.exp(log_ps)
+                    top_p, top_class = ps.topk(1, dim = 1)
+                    equals = top_class == labels.view(*top_class.shape)
+                    accuracy += torch.mean(equals.type(torch.FloatTensor))
+
+                model.train()
+                train_losses.append(losses/len(dataloader_train))
+                test_losses.append(test_losses/len(dataloader_test))
+
+                print("Epoch: {}/{}..".format(epoch+1, epochs),
+                      "Training loss: {:.3f}..".format(running_losses/len(dataloader_train)),
+                      "Test loss: {:.3f}..".format(test_losses/len(dataloader_test)),
+                      "Test Accuracy: {:.3f}".format(accuracy/len(dataloader_test)))
 
 
 
@@ -78,10 +135,13 @@ if __name__ == "__main__":
     # Define configuration parameters
     config = dict()
     config["lr"] = 0.05
+    config["momentum"] = 0.9
+    config["num_classes"] = 10
     config["batchsize_train"] = 64
     config["batchsize_test"] = 64
     config["epochs"] = 3
     config["use_gpu"] = use_gpu
+    config["use_cpu"] = torch.device("cpu")
 
     train_dataset = FashionMNIST_dataset(train=True)
     test_dataset = FashionMNIST_dataset(train=False)
@@ -89,12 +149,10 @@ if __name__ == "__main__":
     train_loader = torch.utils.data.DataLoader(dataaset=train_dataset, batch_size = config["batchsize_train"], shuffle=True)
     test_loader = torch.utils.data.DataLoader(dataaset=test_dataset, batch_size = config["batchsize_test"], shuffle=False)
 
+    optimizer = optim.SGD(model.parameters(), lr=config["lr"], momentum=config["momentum"])
+    criterion = nn.CrossEntropyLoss()
+    scheduler = optim.lr_sceduler.StepLR(optimizer, steo_size=10, gamma=0.1)
 
-
-
-
-
-
-
-
-    HEI
+    net = Network()
+    net.train(net, train_loader, criterion, optimizer, config["epochs"], config["use_cpu"])
+    net.train_eval(model, train_loader, test_loader, criterion, optimizer, config["epochs"], config["use_cpu"])
